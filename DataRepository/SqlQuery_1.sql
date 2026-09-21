56453d750c6a5f8203587b23bb4b8e1afd9d080e
basemsha3bani <79589623+basemsha3bani@users.noreply.github.com>
2026-09-21 22:20:49 +0300

    Add SQL query to insert exam questions

--- DataRepository/SqlQuery_1.sql
+++ DataRepository/SqlQuery_1.sql
@@ -1,1 +1,12 @@
-﻿
+DECLARE @examId INT = 2;
+
+INSERT INTO ExamQuestions (ExamId, QuestionId)
+SELECT @examId, Q.Id
+FROM (
+    SELECT q.Id, q.DifficultyLevelId,
+           ROW_NUMBER() OVER (PARTITION BY q.DifficultyLevelId ORDER BY NEWID()) as rn
+    FROM Questions q
+    WHERE q.DifficultyLevelId IN (SELECT DifficultyLevelId FROM ExamTypesDetails WHERE ExamTypeId = @examType)
+) Q
+JOIN ExamTypesDetails etd ON etd.DifficultyLevelId = Q.DifficultyLevelId AND etd.ExamTypeId = @examType
+WHERE Q.rn <= etd.NumberOfQuestions;﻿
